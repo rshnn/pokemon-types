484c3eed378ceff458e4f55d9ee4d2b826156455
rshn-doomhammer <roshanpatel06@gmail.com>
2020-07-12 12:34:47 -0400

    adds image augmentation.  tried out new model. currently working on exploring other loss functions

--- utils.py
+++ utils.py
@@ -20,6 +20,40 @@ type_encoding = {int(k):v for k, v in type_encoding.items()}
 type_encoding
 
 
+def augment(image):
+    image = tf.image.convert_image_dtype(image, tf.float32)  # Cast and normalize the image to [0,1] 
+    image = tf.image.flip_left_right(image)  
+    image = tf.image.flip_up_down(image)  
+
+    return image 
+
+
+def parse_function_augment(filename, label): 
+    """ Returns a tuple of (normalized image array, label)  
+    Apply transformation as well (this function is for training images).  
+    
+    filename:  string representing path to image 
+    label: multi-hot encoded array of size N_LABELS 
+    """
+    
+    # Read image from file 
+    img_string = tf.io.read_file(filename)
+    
+    # Decode it into a dense vector 
+    img_decoded = tf.image.decode_png(img_string, channels=CHANNELS)
+    
+    # Resize it to a fixed shape 
+    img_resized = tf.image.resize(img_decoded, [IMG_HEIGHT, IMG_WIDTH])
+    
+    # Normalize it to 0-1 scale 
+    img_normed = img_resized / 255.0
+    
+    img_aug = augment(img_normed)  
+        
+    return img_aug, label 
+
+
+
 def parse_function(filename, label): 
     """ Returns a tuple of (normalized image array, label)  
     
@@ -38,15 +72,14 @@ def parse_function(filename, label):
     
     # Normalize it to 0-1 scale 
     img_normed = img_resized / 255.0
-    
+           
     return img_normed, label 
 
 
 
 
-
 def create_dataset(filenames, labels, SHUFFLE_BUFFER_SIZE, 
-					AUTOTUNE, BATCH_SIZE, is_training=True): 
+                    AUTOTUNE, BATCH_SIZE, augment=True): 
     """ Load and parse a tf.data.Dataset.  
     
     filenames: list of image paths 
@@ -55,14 +88,17 @@ def create_dataset(filenames, labels, SHUFFLE_BUFFER_SIZE,
         
     # Create a first dataset of file paths and labels
     dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-    # Parse and preprocess observations in parallel
-    dataset = dataset.map(parse_function, num_parallel_calls=AUTOTUNE)
+    if augment is True:  
+        # Parse and preprocess observations in parallel
+        dataset = dataset.map(parse_function_augment, num_parallel_calls=AUTOTUNE)      
+    else: 
+        dataset = dataset.map(parse_function, num_parallel_calls=AUTOTUNE)
+
     
-    if is_training == True:
-        # This is a small dataset, only load it once, and keep it in memory.
-        dataset = dataset.cache()
-        # Shuffle the data each buffer size
-        dataset = dataset.shuffle(buffer_size=SHUFFLE_BUFFER_SIZE)
+    # This is a small dataset, only load it once, and keep it in memory.
+    dataset = dataset.cache()
+    # Shuffle the data each buffer size
+    dataset = dataset.shuffle(buffer_size=SHUFFLE_BUFFER_SIZE)
         
     # Batch the data for multiple steps
     dataset = dataset.batch(BATCH_SIZE)
@@ -72,17 +108,16 @@ def create_dataset(filenames, labels, SHUFFLE_BUFFER_SIZE,
     return dataset
 
 
-
-
-def plot_prediction_grid(model, dataset, print_types=True):
+def plot_prediction_grid(model, dataset, dims=(3, 3),print_types=True):
     """ Plots a prediction grid given an model and dataset 
     
     model:   tf model 
     dataset:  tf.data.Dataset 
     print_types:  If True, will lookup string names for types 
     """
+    i, j = dims
 
-    fig, axes = plt.subplots(3, 3, figsize=(10, 10))
+    fig, axes = plt.subplots(i, j, figsize=(3*i, 3*j))
     axes.reshape(-1, 1)
 
     for f, l in dataset.take(1):
